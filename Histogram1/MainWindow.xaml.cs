3594780ab4410a11f7f307dea6758068dec24895
Daniel Fosha <dfosha@esri.com>
2017-06-13 16:54:49 -0700

    Reworked histograms

--- Histogram1/MainWindow.xaml.cs
+++ Histogram1/MainWindow.xaml.cs
@@ -1,16 +1,15 @@
 ﻿// Source of Original Code:
 // http://trompelecode.com/blog/2012/04/how-to-create-an-image-histogram-using-csharp-and-wpf/
 
-using AForge.Imaging;
-using System;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Net;
 using System.Windows;
 using System.Windows.Documents;
-using System.Windows.Input;
+// using System.Drawing;
+using Microsoft.Win32;
+using System;
+using System.Windows.Media.Imaging;
 using System.Windows.Media;
+using System.Linq;
 
 namespace Histogram1
 {
@@ -26,17 +25,17 @@ namespace Histogram1
         #endregion
 
         #region Private variables
+
         private String localImagePath = null;
         private PointCollection luminanceHistogramPoints = null;
         private PointCollection redColorHistogramPoints = null;
         private PointCollection greenColorHistogramPoints = null;
         private PointCollection blueColorHistogramPoints = null;
 
-        #endregion
-        
-        #region Public Properties
 
-        public String ImageURL { get; set; }
+        #endregion
+
+        #region Public Properties
 
         public String LocalImagePath
         {
@@ -52,27 +51,6 @@ namespace Histogram1
                     if (this.PropertyChanged != null)
                     {
                         PropertyChanged(this, new PropertyChangedEventArgs("LocalImagePath"));
-                    }
-                }
-            }
-        }
-
-        public bool PerformHistogramSmoothing { get; set; }
-
-        public PointCollection LuminanceHistogramPoints
-        {
-            get
-            {
-                return this.luminanceHistogramPoints;
-            }
-            set
-            {
-                if (this.luminanceHistogramPoints != value)
-                {
-                    this.luminanceHistogramPoints = value;
-                    if (this.PropertyChanged != null)
-                    {
-                        PropertyChanged(this, new PropertyChangedEventArgs("LuminanceHistogramPoints"));
                     }
                 }
             }
@@ -115,7 +93,6 @@ namespace Histogram1
                 }
             }
         }
-
         public PointCollection BlueColorHistogramPoints
         {
             get
@@ -134,6 +111,26 @@ namespace Histogram1
                 }
             }
         }
+        public PointCollection LuminanceHistogramPoints
+        {
+            get
+            {
+                return this.luminanceHistogramPoints;
+            }
+            set
+            {
+                if (this.luminanceHistogramPoints != value)
+                {
+                    this.luminanceHistogramPoints = value;
+                    if (this.PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("LuminanceHistogramPoints"));
+                    }
+                }
+            }
+        }
+
+
 
         #endregion
 
@@ -143,14 +140,7 @@ namespace Histogram1
             InitializeComponent();
             this.DataContext = this;
 
-            try
-            {
-                this.ImageURL = new Uri(System.IO.Path.Combine(Environment.CurrentDirectory, "Sample.jpg"), UriKind.Absolute).AbsoluteUri;
-            }
-            catch
-            {
-                // do nothing, user must enter a URL manually
-            }
+
         }
 
         #endregion
@@ -159,62 +149,64 @@ namespace Histogram1
 
         private void OnWindowLoaded(object sender, RoutedEventArgs e)
         {
-            if (!String.IsNullOrWhiteSpace(this.ImageURL))
-            {
-                OnButtonClick(null, null);
-            }
+
         }
+
+
+
 
         private void OnButtonClick(object sender, RoutedEventArgs e)
         {
-            this.Cursor = Cursors.Wait;
-            try
-            {
-                if (String.IsNullOrWhiteSpace(this.ImageURL))
-                {
-                    MessageBox.Show("Image URL is mandatory.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                    return;
-                }
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files |*.png;*.jpeg;*.jpg;*.tif |All files (*.*)|*.*";
 
-                String localFilePath = null;
-                try
+            if (openFileDialog.ShowDialog() == true)
+            {
+                this.LocalImagePath = openFileDialog.FileName;
+            }
+
+
+            // is this necessary?
+            var source = new BitmapImage(new System.Uri(LocalImagePath));
+            int bitsPerPixel = source.Format.BitsPerPixel;
+
+
+            using (System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(LocalImagePath))
+            {
+
+                int[] red_collection = new int[256];
+                int[] green_collection = new int[256];
+                int[] blue_collection = new int[256];
+                int[] lum_collection = new int[256];
+
+                int stride = bmp.Width * 4;
+                int size = bmp.Height * stride;
+                byte[] pixels = new byte[size];
+
+                for (int i = 0; i < bmp.Height; i++)
                 {
-                    localFilePath = Path.Combine(Path.GetTempPath(), Path.GetTempFileName());
-                    using (WebClient client = new WebClient())
+                    for (int j = 0; j < bmp.Width; j++)
                     {
-                        client.DownloadFile(this.ImageURL, localFilePath);
+                        System.Drawing.Color pixel_color = bmp.GetPixel(j, i);
+
+                        red_collection[pixel_color.R]++;
+                        green_collection[pixel_color.G]++;
+                        blue_collection[pixel_color.B]++;
+                        //this may not be the correct formula
+                        lum_collection[(int)Math.Sqrt(.241 * (pixel_color.R * pixel_color.R) + .691*(pixel_color.G * pixel_color.G ) + .068*(pixel_color.B * pixel_color.B))]++;
                     }
-                    this.LocalImagePath = localFilePath;
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Invalid image URL. Image could not be retrieved", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                    return;
                 }
 
-                using (System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(localFilePath))
-                {
-                    // Luminance
-                    ImageStatisticsHSL hslStatistics = new ImageStatisticsHSL(bmp);
-                    this.LuminanceHistogramPoints = ConvertToPointCollection(hslStatistics.Luminance.Values);
-                    // RGB
-                    ImageStatistics rgbStatistics = new ImageStatistics(bmp);
-                    this.RedColorHistogramPoints = ConvertToPointCollection(rgbStatistics.Red.Values);
-                    this.GreenColorHistogramPoints = ConvertToPointCollection(rgbStatistics.Green.Values);
-                    this.BlueColorHistogramPoints = ConvertToPointCollection(rgbStatistics.Blue.Values);
-                }
+
+                RedColorHistogramPoints = ConvertArrayToPointCollection(red_collection);
+                GreenColorHistogramPoints = ConvertArrayToPointCollection(green_collection);
+                BlueColorHistogramPoints = ConvertArrayToPointCollection(blue_collection);
+                LuminanceHistogramPoints = ConvertArrayToPointCollection(lum_collection);
             }
-            catch(Exception ex)
-            {
-                MessageBox.Show("Error generating histogram : " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            finally
-            {
-                this.Cursor = Cursors.Arrow;
-            }
+            
         }
 
-
+  
         private void OnHyperlinkClick(object sender, RoutedEventArgs e)
         {
             Hyperlink source = sender as Hyperlink;
@@ -223,52 +215,29 @@ namespace Histogram1
                 System.Diagnostics.Process.Start(source.NavigateUri.ToString());
             }
         }
+
+
         #endregion
 
         #region Private Methods
-
-        private PointCollection ConvertToPointCollection(int[] values)
+        private PointCollection ConvertArrayToPointCollection(int[] values)
         {
-            if (this.PerformHistogramSmoothing)
-            {
-                values = SmoothHistogram(values);
-            }
-
             int max = values.Max();
 
             PointCollection points = new PointCollection();
             // first point (lower-left corner)
             points.Add(new Point(0, max));
             // middle points
-            for (int i = 0; i < values.Length; i++)
+            for (int i =0; i < values.Length; i++)
             {
                 points.Add(new Point(i, max - values[i]));
             }
-            // last point (lower-right corner)
+            // last point (lower right corner)
             points.Add(new Point(values.Length - 1, max));
 
             return points;
+
         }
-
-        private int[] SmoothHistogram(int[] originalValues)
-        {
-            int[] smoothedValues = new int[originalValues.Length];
-
-            double[] mask = new double[] { 0.25, 0.5, 0.25 };
-
-            for (int bin = 1; bin < originalValues.Length - 1; bin++)
-            {
-                double smoothedValue = 0;
-                for (int i = 0; i < mask.Length; i++)
-                {
-                    smoothedValue += originalValues[bin - 1 + i] * mask[i];
-                }
-                smoothedValues[bin] = (int)smoothedValue;
-            }
-
-            return smoothedValues;
-        }
-
         #endregion
     }
 }
